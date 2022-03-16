c19cc46bd36cdc2e8471b7b395a205c201842515
Anuj Singh <anujZzztw@gmail.com>
2022-03-16 18:33:06 +0530

    done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'idk': '#f5f5f5',
+      },
+      margin: {
+        '14vw': '14vw',
+      }
+
+    },
+  },
+  plugins: [],
+}
